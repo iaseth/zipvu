f34f5e2cfcf0e8cff69afda87c04bd1ef6ec6ec3
Ankur Seth <iankurseth@gmail.com>
2022-03-01 20:34:17 +0530

    Now displaying all JPG images from zip

--- src/Zipvu/Zipvu.js
+++ src/Zipvu/Zipvu.js
@@ -17,11 +17,26 @@ export default function Zipvu () {
 
 	const contentItems = contents.map((c, i) => {
 		const f = zip.files[c];
+		const is_image = c.endsWith(".jpg");
+
+		const image_id = `image_${i}`;
+		if (is_image) {
+			f.async("blob").then((blob) => {
+				let src = URL.createObjectURL(blob);
+				document.getElementById(image_id).src = src;
+			});
+		}
+
 		return (
 			<div key={i} className="flex bg-slate-50 border border-slate-300 text-slate-800 text-sm font-bold">
 				<div className="px-3 py-3 w-16 border-r-2 border-slate-300 text-right">{i+1}</div>
 				<div className="px-3 py-3">
-					<span className="px-2 py-1 bg-slate-200">{c}</span>
+					<div>
+						<span className="px-2 py-1 bg-slate-200">{c}</span>
+					</div>
+					{is_image && <div className="py-4">
+						<img id={image_id} src="#" className="max-w-md p-2 border-2 border-blue-500" />
+					</div>}
 				</div>
 			</div>
 		);
