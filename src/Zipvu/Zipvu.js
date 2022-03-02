fb3c7cab85356f81d5833c75e768c6337cfe9138
Ankur Seth <iankurseth@gmail.com>
2022-03-02 14:59:11 +0530

    Corrected problems with image display

--- src/Zipvu/Zipvu.js
+++ src/Zipvu/Zipvu.js
@@ -17,25 +17,27 @@ export default function Zipvu () {
 
 	const contentItems = contents.map((c, i) => {
 		const f = zip.files[c];
-		const is_image = c.endsWith(".jpg");
+		if (!f) return;
+		const is_image = c.endsWith(".jpg") || c.endsWith(".jpeg") || c.endsWith(".png");
 
 		const image_id = `image_${i}`;
 		if (is_image) {
 			f.async("blob").then((blob) => {
 				let src = URL.createObjectURL(blob);
-				document.getElementById(image_id).src = src;
+				let element = document.getElementById(image_id);
+				if (element) element.src = src;
 			});
 		}
 
 		return (
-			<div key={i} className="flex bg-slate-50 border border-slate-300 text-slate-800 text-sm font-bold">
+			<div key={i} className="flex bg-slate-50 mb-1 border border-slate-300 text-slate-800 text-sm font-bold">
 				<div className="px-3 py-3 w-16 border-r-2 border-slate-300 text-right">{i+1}</div>
-				<div className="px-3 py-3">
+				<div className="px-3 py-3 overflow-hidden">
 					<div>
-						<span className="px-2 py-1 bg-slate-200">{c}</span>
+						<span className="px-2 py-1 bg-slate-200 font-mono">{c}</span>
 					</div>
 					{is_image && <div className="py-4">
-						<img id={image_id} src="#" className="max-w-md p-2 border-2 border-blue-500" />
+						<img id={image_id} src="#" alt="" className="max-w-full p-2 border-2 border-blue-500" />
 					</div>}
 				</div>
 			</div>
